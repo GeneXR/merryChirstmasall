dcea381241d7d74ebd9935a1b054a3602916b2d5
Lisa <7e4a2944@student.curtin.edu.my>
2021-12-25 18:10:27 +0800

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+(function ($) {
+  // setTimeout(function () {
+  //   $(".coverBg").addClass("inactive");
+  //   $(".contentBg").addClass("active");
+  // }, 3000);
+  $(".coverBg").on("click", function () {
+    $(".coverBg").addClass("inactive");
+    $(".contentBg").addClass("active");
+  });
+})(jQuery);
